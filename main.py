70a83e2d38d9e60eb11aa2d7f99f963d76fd6586
nedved942 <pawelkoziol77@wp.pl>
2023-11-11 23:46:15 +0100

    Dodanie dekoratora obliczającego czas wykonania funkcji, a także zapisanie rezultatów do pliku results.txt

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+from time import time
+from datetime import datetime
+from functools import wraps
+
 print("*** Mierzenie czasu wykonania silni ***")
 
 
+def operation_time(callback):
+    @wraps(callback)
+    def wrapper(number):
+        # start_time = datetime.now()
+        start_time = time()
+        callback(number)
+        # end_time = datetime.now()
+        end_time = time()
+        time_of_operation = end_time - start_time
+        return time_of_operation
+    return wrapper
+
+
+@operation_time
 def calculate_factorial(number):
     function_sum = 1
     for i in range(1, number + 1):
@@ -10,4 +28,13 @@ def calculate_factorial(number):
     return function_sum
 
 
-print(calculate_factorial(10))
+print(calculate_factorial(11000))
+
+with open("results.txt", "w") as file_stream:
+    required_factorial_values = [1, 9, 27, 88, 175, 299, 512, 1024]
+    for value in required_factorial_values:
+        file_stream.write(f"Czas obliczenia silni dla wartości {value} wynosi {calculate_factorial(value)}s.")
+        file_stream.write("\n")
+
+
+
