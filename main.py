e9d5072c11f451e62c4a1a9fef301da73ee95401
nedved942 <pawelkoziol77@wp.pl>
2023-11-12 01:02:50 +0100

    Dodanie funkcji z cache, obliczenie czasu wykonania za pomocą perf_counter i uporządkowanie kodu.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from time import time
-from datetime import datetime
-from functools import wraps
+from time import perf_counter
+from functools import wraps, cache
 
 print("*** Mierzenie czasu wykonania silni ***")
 
@@ -8,11 +7,9 @@ print("*** Mierzenie czasu wykonania silni ***")
 def operation_time(callback):
     @wraps(callback)
     def wrapper(number):
-        # start_time = datetime.now()
-        start_time = time()
+        start_time = perf_counter()
         callback(number)
-        # end_time = datetime.now()
-        end_time = time()
+        end_time = perf_counter()
         time_of_operation = end_time - start_time
         return time_of_operation
     return wrapper
@@ -22,19 +19,35 @@ def operation_time(callback):
 def calculate_factorial(number):
     function_sum = 1
     for i in range(1, number + 1):
-        function_sum = function_sum * i
+        function_sum *= i
         # print(f"Numer iteracji to: {i}.")
         # print(f"Suma wynosi: {function_sum}.")
     return function_sum
 
 
-print(calculate_factorial(11000))
+@cache
+@operation_time
+def calculate_factorial_with_cache(number):
+    function_sum = 1
+    for i in range(1, number + 1):
+        function_sum *= i
+        # print(f"Numer iteracji to: {i}.")
+        # print(f"Suma wynosi: {function_sum}.")
+    return function_sum
+
 
 with open("results.txt", "w") as file_stream:
     required_factorial_values = [1, 9, 27, 88, 175, 299, 512, 1024]
     for value in required_factorial_values:
-        file_stream.write(f"Czas obliczenia silni dla wartości {value} wynosi {calculate_factorial(value)}s.")
+        file_stream.write(f"Czas obliczenia silni dla wartości {value} wynosi {calculate_factorial(value):.25f}s.")
         file_stream.write("\n")
 
 
+with open("results_with_cache.txt", "w") as file_stream:
+    required_factorial_values = [1, 9, 27, 88, 175, 299, 512, 1024]
+    for value in required_factorial_values:
+        file_stream.write(f"Czas obliczenia silni dla wartości {value} "
+                          f"wynosi {calculate_factorial_with_cache(value):.25f}s.")
+        file_stream.write("\n")
 
+print("Polecenie wykonano pomyślnie.")
